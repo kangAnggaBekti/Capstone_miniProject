f834458dba59aa8c86ee796920399bea3ec90a7c
kangAnggaBekti <originalkangangga@gmail.com>
2024-02-01 14:11:23 +0700

    tambahan keterangan dalam dashboard

--- resources/views/anggota/dashboard/index.blade.php
+++ resources/views/anggota/dashboard/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.stisla.index', ['title' => 'Anggota Dashboard', 'section_header' => 'Anggota Dashboard'])
 
 @section('content')
+<div class="flexible-container" id="text-container">
+  <!-- You can replace the text content below with your own text -->
+  <h2><b>Selamat Datang Di Perpustakaan Gratis Kami</b></h2>
+  <br>
+  <h4><b>Silahkan pilih menu yang tersedia di sidebar</b></h4>
+  <br>
+  <h5><b>Tentang Kami</b></h5>
+  <br>
+  <h6>Kami menyadari bahwa minat baca bangsa indonesia saat ini masih dibawah rata-rata jika dibandingkan negara tetangga diasia tenggara.<br>
+  Maka dari itu kami membuat website perpustakaan gratis ini untuk anda dan menyediakan konten-konten yang bermanfaat secara gratis.</h6>
+</div><br>
 <div class="row">
   <div class="col-lg-4 col-md-4 col-sm-12">
     <div class="card card-statistic-2">
@@ -33,7 +44,17 @@
       </div>
     </div>
   </div>
+<br>
+<div class="flexible-container" id="text-container">
+  <h4><b>Tata-Tertib</b></h4>
+  <h6>- Harap untuk mengembalikan buku tepat waktu</h6><br>
+  <h6>- Harap tidak merusak buku yang dipinjam</h6><br>
+  <h6>- Harap merawat buku yang sudah dipinjam dengan baik</h6>
+</div>
+</div>
 
-  </div>
+
+</div>
+
 </div>
 @endsection
